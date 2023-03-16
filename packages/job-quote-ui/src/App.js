2b26af2c364751c8767091a6a91cf8be29a43c21
Rupesh Ravindran <rupesh.ravindran@stonex.com>
2023-03-16 19:39:01 +0000

    router added

--- packages/job-quote-ui/src/App.js
+++ packages/job-quote-ui/src/App.js
@@ -3,21 +3,44 @@ import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import './App.css';
 import construction from './img/construction.webp';
+import { Link, Routes, Route } from 'react-router-dom'; 
 
 function App() {
   return (
     <div className="App">
       <header>
-        <nav>
+        {/* <nav>
           <ButtonGroup variant="contained" aria-label="outlined primary button group">
-            <Button>Post a Job</Button>
+            <Button >Post a Job</Button>
             <Button>My Posted Jobs</Button>
             <Button>Advice Center</Button>
             <Button>My Accounts</Button>
           </ButtonGroup>       
-        </nav>
+        </nav> */}
+         <nav>
+        <ul>
+          <li>
+            <Link to="/post-job">POST A JOB</Link>
+          </li>
+          <li>
+            <Link to="/my-posted-job">MY POSTED JOB</Link>
+          </li>
+          <li>
+            <Link to="/advice-center">ADVICE CENTER</Link>
+          </li>
+          <li>
+            <Link to="/my-accounts">MY ACCOUNTS</Link>
+          </li>
+        </ul>
+      </nav>
       </header>
       <main>
+      <Routes>
+        <Route path="/post-job" element={<PostJob />} />
+        <Route path="/my-posted-job" element={<MyPostedJob />} />
+        <Route path="/advice-center" element={<AdviceCenter />} />
+        <Route path="/my-accounts" element={<MyAccounts />} />
+      </Routes>
         <div className='App-body'>
           <div></div>
           <div>
@@ -30,6 +53,22 @@ function App() {
       </footer>
     </div>
   );
+}
+
+function PostJob() {
+  return <h2>POST A JOB</h2>;
+}
+
+function MyPostedJob() {
+  return <h2>MY POSTED JOB</h2>;
+}
+
+function AdviceCenter() {
+  return <h2>ADVICE CENTER</h2>;
+}
+
+function MyAccounts() {
+  return <h2>MY ACCOUNTS</h2>;
 }
 
 export default App;
